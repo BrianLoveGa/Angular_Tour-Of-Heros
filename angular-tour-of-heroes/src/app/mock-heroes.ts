1d8ac4fd2511ce6762d607c4b93359405afe127f
BLoveless <bloveless@talentpath.com>
2020-09-01 17:29:34 -0400

    found some heroes

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr. Nice' },
+  { id: 12, name: 'The Blue Raja' },
+  { id: 13, name: 'The Bowler' },
+  { id: 14, name: 'Captain Amazing' },
+  { id: 15, name: 'The Shoveler' },
+  { id: 16, name: 'Invisible Boy' },
+  { id: 17, name: 'Dr. Anabel Leek' },
+  { id: 18, name: 'Spleen' },
+  { id: 19, name: 'Mr. Furious' },
+  { id: 20, name: 'Sphinx' }
+];
